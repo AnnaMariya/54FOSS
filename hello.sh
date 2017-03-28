13e41202b7f183125f86121b0cd7281108b6869e
AnnaMariya <annamariyakurishingal@gmail.com>
2017-03-28 15:42:38 +0530

    Adding file

---
+++ hello.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+<<<<<<< HEAD
+a=100
+b=200
+let c=a+b
+echo "Value of c=$c"
+d=$(expr 5 \* $1)
+echo $d
+e=jesus 
+f=christ
+g=$e$f
+echo $g
+
+=======
+echo HELLO WORLD!!!
+>>>>>>> d035967c262336b0096cb65b9971d3e7ecbd0ab6
